ffea24e661c48dfa7fbe6f91dfdfe8d277f2d5f1
Dipak kumar <134565424+duser77@users.noreply.github.com>
2024-03-30 16:18:59 +0530

    src and json

---
+++ src/Pricing/PricingBox.js
@@ -0,0 +1,22 @@
+import './PricingBox.css'
+export default function PricingBox()  {
+  return (
+    <div className="pricingBoxMain">
+      <div className="pricingBoxCont">
+            <div className="pricingBox w-[320px]">
+                <div className='flex w-[320px] h-[50px] font-mono justify-center items-center  '>
+                    Free
+                </div>
+                <div className='flex w-[320px] h-[80px] font-mono justify-center items-center text-xl font-bold'>
+                    <img className='h-[50px]' src="/p1.png" alt="p1" /> /months
+                </div>
+            </div>
+            <div className="pricingBox">
+            
+            </div>
+      </div>
+    </div>
+  )
+}
+
+ 
